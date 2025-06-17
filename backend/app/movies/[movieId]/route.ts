234c515d246f9444a71a3e7fec1e648ff5b1ab61
Pratik Devkota <114279879+Pratik58D@users.noreply.github.com>
2025-06-17 12:51:34 +0545

    feat:patch , delete , searching

--- backend/app/movies/[movieId]/route.ts
+++ backend/app/movies/[movieId]/route.ts
@@ -1,15 +1,67 @@
 import { movies } from "../db";
 
+interface MovieParams {
+    params:{
+    movieId : string
+       
+    }
+}
+
 // using _req cause we are not using the req right noe
 export async function GET(
-    _req : Request,
-    {params} : {params : {movieId : string}}
-){
-    const {movieId} = await params;
-    // add +movies to covert string into number Number(movieId)
-    const movie = movies.find((movie)=> movie.id === +movieId);
-    return movie ? new Response(JSON.stringify(movie)) :
-    new Response ("Movie not Found", {status : 404})
+  _req: Request,
+  { params }: MovieParams
+) {
+  const { movieId } = await params;
+  // add +movies to covert string into number Number(movieId)
+  const movie = movies.find((movie) => movie.id === +movieId);
+  return movie
+    ? new Response(JSON.stringify(movie))
+    : new Response("Movie not Found", { status: 404 });
+}
+
+//updating the movie
+export async function PATCH(
+  req: Request,
+  { params }: { params: { movieId: string } }
+) {
+  const { movieId } = params;
+  const movie = movies.find((movie) => movie.id === +movieId);
+  if (!movie) {
+    return new Response(JSON.stringify({ error: "Movie Not Found" }), {
+      status: 404,
+    });
+  }
+
+  try {
+    const updatedMovie = await req.json();
+    //find the index of the movie
+    const index = movies.findIndex((m) => m.id === +movieId);
+   
+  //update the movie
+  movies[index] = {...movie, ...updatedMovie}
+  return new Response(JSON.stringify(movies[index]), {status : 200})
+
+  } catch (error) {
+    return new Response(JSON.stringify({ error }), { status: 400 });
+  }
 }
 
 
+//delete the movies
+
+export async function DELETE(req : Request, {params}: MovieParams){
+    const {movieId} = params;
+    const ToDeleteMovieIndex = movies.findIndex(m => m.id === +movieId);
+    console.log("deleted Index:",ToDeleteMovieIndex)
+    if (ToDeleteMovieIndex === -1){
+        return new Response(JSON.stringify({error : "movie not found"}  ), {status : 404})
+    };
+    movies.splice(ToDeleteMovieIndex,1)
+
+
+    return new Response(JSON.stringify({message :"deleted!"}), {status : 202})
+
+
+
+}
--- backend/app/movies/route.ts
+++ backend/app/movies/route.ts
@@ -1,12 +1,13 @@
+import { NextRequest } from "next/server";
 import { movies } from "./db";
 
 // export async function GET(){
 //     return new Response("hello World , i am Pratik Devkota");
 // }
 
-export async function GET(){
-    return Response.json(movies);
-}
+// export async function GET(){
+//     return Response.json(movies);
+// }
 
 export async function POST(req : Request) {
     const movie = await req.json();
@@ -15,4 +16,19 @@ export async function POST(req : Request) {
     console.log(newMovie);
     movies.push(newMovie);
     return new Response(JSON.stringify(newMovie))
+}
+
+
+//searching for the movie
+
+export async function GET(req:NextRequest) {
+    // console.log(req.nextUrl.searchParams);
+    const searchParams = req.nextUrl.searchParams;
+    const query = searchParams.get("query");
+    // console.log("query is :",query) //john
+    const filterMovies = query 
+    ? movies.filter((m)=> m.name.toLowerCase().includes(query)) 
+    : movies;
+    
+    return new Response(JSON.stringify(filterMovies))
 }
